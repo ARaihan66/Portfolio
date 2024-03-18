7b5ef7feca368ccbc6cc5c013e7bcd910b361e21
Abu Raihan <aburaihancse066@gmail.com>
2024-03-18 12:55:47 +0600

    Tailwind css and router added

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <div>Portfolio</div>
+      <div>
+        <ul>
+          <li>Home</li>
+          <li>Skills</li>
+          <li>Projects</li>
+          <li>Contacts</li>
+        </ul>
+      </div>
+      <div>
+        <a href="/">Github Link</a>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
