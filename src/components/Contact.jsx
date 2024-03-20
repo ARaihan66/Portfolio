03c1a1b3c3d8b05f659575a66dc5af2d69e8c6d4
Abu Raihan <aburaihancse066@gmail.com>
2024-03-21 04:05:48 +0600

    Add EmailJS

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Email from "../file_animation/Email.json";
+import Globe from "../file_animation/Globe.json";
 import Lottie from "react-lottie";
 import facebook from "../assets/facebook.png";
 import linkedin from "../assets/linkedin.png";
@@ -12,29 +12,45 @@ import {
   Tab,
   TabPanel,
 } from "@material-tailwind/react";
+import { useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 const Contact = () => {
   const [type, setType] = useState("message");
   const defaultSendMessage = {
     loop: true,
     autoplay: true,
-    animationData: Email,
+    animationData: Globe,
     rendererSettings: {
       preserveAspectRatio: "xMidYMid slice",
     },
   };
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm("service_yof2bos", "template_as79n4m", form.current, {
+        publicKey: "795fKXV1nYWFOGoTA",
+      })
+      .then(
+        () => {
+          console.log("SUCCESS!");
+        },
+        (error) => {
+          console.log("FAILED...", error.text);
+        }
+      );
+  };
+
   return (
     <section id="contact" className="py-10">
       <h2 className="uppercase text-center my-5 md:my-10  text-white">
         Contact
       </h2>
       <div className="flex flex-wrap flex-col justify-center items-center bgBlue p-5 md:flex-row md:p-15">
-        <div className=" basis-1/2 flex justify-center items-center">
-          <div className="w-[300px] h-[300px] md:w-[500px] md:h-[500px] p-10 md:mt-10 lg:mt-0  lg:ml-20 ">
-            <Lottie options={defaultSendMessage} />
-          </div>
-        </div>
-
         <div className="basis-1/2">
           <Card className=" w-[330px] bg-indigo-900 md:w-full">
             <CardBody>
@@ -70,14 +86,20 @@ const Contact = () => {
                   }}
                 >
                   <TabPanel value="message" className="p-0 h-[360px]">
-                    <form className="mt-12 flex flex-col gap-5 text-white">
+                    <form
+                      ref={form}
+                      onSubmit={sendEmail}
+                      className="mt-12 flex flex-col gap-5 text-white"
+                    >
                       <input
                         type="text"
+                        name="from_name"
                         placeholder="Enter Name"
                         className="border-none py-5 px-10 outline-none rounded-lg"
                       />
                       <input
                         type="email"
+                        name="from_email"
                         placeholder="Enter Your Mail"
                         className="border-none py-5 px-10 outline-none rounded-lg"
                       />
@@ -85,12 +107,14 @@ const Contact = () => {
                         type="text"
                         cols="5"
                         rows="5"
+                        name="message"
                         placeholder="Message"
                         className="border-none py-5 px-10 outline-none rounded-lg"
                       />
                       <input
                         type="submit"
-                        placeholder="Enter Your Mail"
+                        value="Submit"
+                        onClick={() => window.location.reload()}
                         className="border-none font-bold bg-orange-400 py-5 px-10 outline-none rounded-lg cursor-pointer hover:bg-white"
                       />
                     </form>
@@ -125,6 +149,11 @@ const Contact = () => {
               </Tabs>
             </CardBody>
           </Card>
+        </div>
+        <div className=" basis-1/2 flex justify-center items-center">
+          <div className="w-[300px] h-[300px] md:w-[500px] md:h-[500px] p-10 md:mt-10 lg:mt-0  lg:ml-20 ">
+            <Lottie options={defaultSendMessage} />
+          </div>
         </div>
       </div>
     </section>
